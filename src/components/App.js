34cd348a297072788d51efdc6b7cd4de88dac5a8
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-05 01:07:38 +0600

    Quiz Page Done
    Video : 33

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import "../styles/App.css";
 import Layout from "./Layout";
+import Quiz from "./pages/Quiz";
 // import Home from "./pages/Home";
 // import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
+      <Quiz />
     </Layout>
   );
 }
