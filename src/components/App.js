b525cad5b722b84dc183612ac65ab40f6f46b749
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-09 22:01:25 +0600

    fetch Questions
    Video : 40

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
 import "../styles/App.css";
 import Layout from "./Layout";
-import Result from "./pages/Result";
-import Quiz from "./pages/Quiz";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {AuthProvider} from "../contexts/AuthContext";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 
@@ -15,15 +15,13 @@ function App() {
     <Router>
       <AuthProvider>
         <Layout>
-          
           <Switch>
-              <Route exact path="/" component={Home} />
-              <PublicRoute exact path="/signup" component={Signup} />
-              <PublicRoute exact path="/login" component={Login} />
-              <PrivateRoute exact path="/quiz" component={Quiz} />
-              <PrivateRoute exact path="/result" component={Result} />
+            <Route exact path="/" component={Home} />
+            <PublicRoute exact path="/signup" component={Signup} />
+            <PublicRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/quiz/:id" component={Quiz} />
+            <PrivateRoute exact path="/result/:id" component={Result} />
           </Switch>
-
         </Layout>
       </AuthProvider>
     </Router>
