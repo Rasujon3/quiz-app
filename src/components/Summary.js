c3ec58a49853d094c2149cf5d7e5fe8495fa1e64
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-10 03:32:45 +0600

    summary dynamic using fatch api
    Video : 43

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,18 +1,51 @@
-import image from "../assets/images/success.png";
+import { useMemo } from "react";
+import successImage from "../assets/images/success.png";
+import useFetch from "../hooks/useFetch";
 import classes from "../styles/Summary.module.css";
 
-export default function Summary({score ,noq}) {
+export default function Summary({ score, noq }) {
+const REACT_APP_PEXELS_API_KEY='563492ad6f917000010000015ccdc3bc6e0d4bee906977921c040d6b';
+
+  const getKeyword = useMemo(() => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return "failed";
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return "good";
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return "very good";
+    } else {
+      return "excellent";
+    }
+  }, [score, noq]);
+
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getKeyword}&per_page=1`,
+    "GET",
+    {
+      Authorization: REACT_APP_PEXELS_API_KEY,
+    }
+  );
+
+  const image = result ? result?.photos[0].src.medium : successImage;
+
   return (
     <div className={classes.summary}>
       <div className={classes.point}>
         <p className={classes.score}>
-          Your score is <br />{score} out of {noq * 5}
+          Your score is <br />
+          {score} out of {noq * 5}
         </p>
       </div>
 
-      <div className={classes.badge}>
-        <img src={image} alt="Success" />
-      </div>
+      {loading && <div className={classes.badge}>Loading your badge...</div>}
+
+      {error && <div className={classes.badge}>An error occured!</div>}
+
+      {!loading && !error && (
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      )}
     </div>
   );
 }
